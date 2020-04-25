c6f553ddbb897ff08edb39a9374b53e7ad80f79a
TracyChen33 <tracychen3621@gmail.com>
2020-04-24 21:24:03 -0600

    deploy

---
+++ precache-manifest.88cff1f54de04dd56eb704f50258ae70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5032c51930ca430d18e300a7210fa943",
+    "url": "/Morney-website/QRcode.png"
+  },
+  {
+    "revision": "f169bb14d4baf54b834f",
+    "url": "/Morney-website/css/app.4179b3d5.css"
+  },
+  {
+    "revision": "5c0be9ea5c47f2055299d52e2131cb1b",
+    "url": "/Morney-website/index.html"
+  },
+  {
+    "revision": "f169bb14d4baf54b834f",
+    "url": "/Morney-website/js/app.b757b60f.js"
+  },
+  {
+    "revision": "21276f84254a8608d1a1",
+    "url": "/Morney-website/js/chunk-vendors.30ec3dda.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-website/robots.txt"
+  }
+]);
